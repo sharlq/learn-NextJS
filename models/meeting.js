eba6149b76defd159acd5a5e7966eb92e2bc88b6
sharlq <shehabakhader@gmail.com>
2021-06-02 14:49:53 +0300

    solved the problem caused by the confilct of the next js loader and the mongoose model

--- models/meeting.js
+++ models/meeting.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
 
+const MODEL_NAME = "myMeetings"
 const meeting = new mongoose.Schema({
     image:String,
     title:String,
     address:String
 })
 
-export default mongoose.model('meetings', meeting)
+export default mongoose.models[MODEL_NAME] || mongoose.model(MODEL_NAME, meeting,'meetings')
